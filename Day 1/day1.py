461947db3c12a0f07589f4dfabec3bae4beda5d6
JMAGoossens <goossens.jma@gmail.com>
2019-12-01 12:38:15 +0100

    Day 1 cleaner code

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -5,14 +5,14 @@ def calc(nr):
     return result + calc(result)
 
 
-def sumup():
-    lines = open("input.txt").read().split('\n')
-    lines = list(filter(None, lines))
-    sum = 0
+def sumup(lines):
+    fuel = 0
     for line in lines:
-        sum += calc(int(line))
-    return sum
+        fuel += calc(int(line))
+    return fuel
 
 
-print(sumup())
-# 5068326 is too low
+lines = open("input.txt").read().split('\n')
+lines = list(filter(None, lines))
+
+print(sumup(lines))
