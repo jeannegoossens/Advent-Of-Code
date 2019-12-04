91f9bf4800976491c5242f5e0354cdeb68a34467
JMAGoossens <goossens.jma@gmail.com>
2019-12-04 08:23:04 +0100

    not working but why?

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -1,26 +1,33 @@
 low = 136818
 high = 685979
 
-def checkPasswor(pw):
+
+def checkPassword(pw):
     if len(str(pw)) != 6:
         return False
 
     same = 0
+    current = 0
     last = ''
     for char in str(pw):
         if char < last:
             return False
         if last == char:
-            same += 1
+            current += 1  # current buildup of the same character in a row
+        else:  # the chain is broken
+            if current == 2:  # if there were exactly two, count the occurence
+                same += 1
+            current = 0  # reset current chain
         last = char
 
-    if same == 0:
-        return False
-    return True
+    if same > 0:  # if there was at least one occurences of 2 equal characters in a row
+        return True
+    return False
+
 
 count = 0
 for x in range(low, high):
-    if checkPasswor(x):
+    if checkPassword(x):
         count += 1
 
 print(count)
