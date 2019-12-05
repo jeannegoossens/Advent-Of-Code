b4d00a0c6ff634d99d4e8c664229f992d9bbca86
jeannegoossens <45682783+jeannegoossens@users.noreply.github.com>
2019-12-05 15:10:57 +0100

    Part 1 (incomplete)

---
+++ Day 5/day5.py
@@ -0,0 +1,37 @@
+def interpret(opcode):
+    opcode = str(opcode).zfill(5)
+    op = int(opcode[-2:])
+    p1 = int(opcode[-3])
+    p2 = int(opcode[-4])
+    p3 = 0  # always 0
+    return (p3, p2, p1, op)
+
+def getvalue(numbers, i):
+    print(numbers[i])
+    print(numbers[:15])
+    while str(numbers[i]).zfill(5)[-2:] != 99:
+        opcode = interpret(numbers[i])
+        
+        print(opcode, i)
+        print(numbers[:15], '225:', numbers[225])
+        
+        first  = numbers[numbers[i+1]] if opcode[2] == 0 else numbers[i+1]
+        second = numbers[numbers[i+2]] if opcode[1] == 0 else numbers[i+2]
+        third  = numbers[i+3]
+
+        if opcode[3] == 1:
+            numbers[third] = first + second
+            i += 4
+        elif opcode[3] == 2:
+            numbers[third] = first * second
+            i += 4
+        elif opcode[3] == 3:
+            numbers[numbers[i+1]] = int(input('Geef een input: '))
+            i += 2
+        elif opcode[3] == 4:
+            print(first)
+            i += 2
+
+numbers = open('input.txt').read().split(',')
+numbers = [int(i) for i in numbers]
+getvalue(numbers, 0)
