2774035cf5e52b422b86fa32e1566aace8c52282
jeannegoossens <45682783+jeannegoossens@users.noreply.github.com>
2019-12-06 13:40:15 +0100

    Day 6 first part

---
+++ Day 6/day6.py
@@ -0,0 +1,19 @@
+import networkx as nx
+
+lines = open('input.txt').read().split('\n')
+
+# read all edges from this input
+edges = []
+for x in lines:
+    sentence = x.split(')')
+    edges.append((sentence[0], sentence[1]))
+    
+# create a graph and add the edges
+G = nx.Graph()
+G.add_edges_from(edges)
+
+# part 1
+s = 0
+for n in G.nodes:
+    s += len(nx.shortest_path(G, source='COM', target=n)) - 1
+print(s)
