c9723b1368f146f061da35efd7340efe3ba7d720
JMAGoossens <goossens.jma@gmail.com>
2019-12-08 10:01:22 +0100

    day 8 part two

--- Day 8/script.py
+++ Day 8/script.py
@@ -8,6 +8,23 @@ least = image
 for l in layers:
     if l.count('0') < least:
         least = l.count('0')
-        print(layers.index(l))
 
 print(layers[6].count('1') * layers[6].count('2'))
+
+# 0 = black
+# 1 = white
+# 2 = transparent
+
+finalcanvas = ['0' for i in range(image)]
+
+for l in reversed(layers):
+    for i in range(image):
+        if l[i] == '0':
+            finalcanvas[i] = ' '
+        elif l[i] == '1':
+            finalcanvas[i] = 'X'
+
+finalcanvas = ''.join(finalcanvas)
+finalcanvas = [finalcanvas[i:i+25] for i in range(0, len(finalcanvas), 25)]
+for f in finalcanvas:
+    print(f)
