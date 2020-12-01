a9045577b9cb3f224f66a1adfe1674a9c40de839
jeannegoossens <45682783+jeannegoossens@users.noreply.github.com>
2020-12-01 10:44:08 +0100

    Update script.py

--- 2020/Day01/script.py
+++ 2020/Day01/script.py
@@ -6,4 +6,3 @@ input = list(map(int, open('input.txt').read().split('\n')))
 for x in itertools.combinations(input, 3):
     if sum(x) == 2020:
         print('found combination', x, 'with product', np.prod(x))
-        break
