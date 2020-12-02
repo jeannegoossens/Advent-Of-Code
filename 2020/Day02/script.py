2a368c7e559d9b42c6765a36a71ac8881f4908fa
jeannegoossens <45682783+jeannegoossens@users.noreply.github.com>
2020-12-02 11:19:08 +0100

    day02 - simplified code

--- 2020/Day02/script.py
+++ 2020/Day02/script.py
@@ -1,29 +1,22 @@
 input = open('input.txt').read().split('\n')
 
-
-def check_password(least, most, letter, password):
-    amount = password.count(letter)
-    if least <= amount <= most:
-        return True
-    else:
-        return False
-
-
 correct_first = 0
 correct_second = 0
+
 for line in input:
     rule, letter, password = line.replace(':', '').split(' ')
     least, most = map(int, rule.split('-'))
 
     # for part 1: count the occurrence of the letter in the password
     # count should be between (including) boundaries
-    if check_password(least, most, letter, password):
+    if least <= password.count(letter) <= most:
         correct_first += 1
 
     # for part 2: count only the occurrence of the letter in the given indices
     # count should be exactly 1
     password = password[least - 1] + password[most - 1]
-    if check_password(1, 1, letter, password):
+    if password.count(letter) == 1:
         correct_second += 1
+
 print('answer to part 1:', correct_first)
 print('answer to part 2:', correct_second)
