ccd842387208cb50089aa9019e53d2004da836e6
JMAGoossens <goossens.jma@gmail.com>
2020-12-06 09:15:54 +0100

    solution day 6: re-add part 1

--- 2020/Day06/script.py
+++ 2020/Day06/script.py
@@ -2,15 +2,19 @@ input = open('input.txt').read().split('\n\n')
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-total = 0
+total_1 = 0
+total_2 = 0
 for group in input:
     answers = group.split('\n')
     for letter in alphabet:
+        if group.count(letter) > 0:
+            total_1 += 1
         everyone = 0
         for person in answers:
             if person.count(letter) > 0:
                 everyone += 1
         if everyone == len(answers):
-            total += 1
+            total_2 += 1
 
-print(total)
+print('solution to part 1:', total_1)
+print('solution to part 2:', total_2)
