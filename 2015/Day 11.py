e7bc483897d18648d790ffe327d98cd730afa5ad
jeannegoossens <45682783+jeannegoossens@users.noreply.github.com>
2020-12-08 17:23:16 +0100

    solution to 2015 day 11 fixed indexerror

--- 2015/Day 11.py
+++ 2015/Day 11.py
@@ -1,13 +1,3 @@
-# 2015
-# Day 11
-
-# 8 lowercase
-# 1 increasing straight of at least 3 letters
-# may not contain i, o, or l
-# must contain at least 2 different, non-overlapping pairs of letters
-
-input = 'cqjxjnds'
-
 alphabet = list('abcdefghijklmnopqrstuvwxyz')
 
 
@@ -17,18 +7,15 @@ def checkrules(password):
     for x in range(len(password)):
         if password[x] in ['l', 'i', 'o']:
             return False
-        try:
-            if password[x] == password[x+1] and password[x] != password[x+2]:
-                count_doubles += 1
-        except IndexError:
-            break
+        if password[x] == password[x+1] and (x+2 >= len(password) or password[x] != password[x+2]):
+            count_doubles += 1
         if straight is False and alphabet.index(password[x+1]) - alphabet.index(password[x]) == 1:
             try:
                 if alphabet.index(password[x+2]) - alphabet.index(password[x+1]) == 1:
                     straight = True
             except IndexError:
                 break
-    if straight is False or count_doubles < 2:
+    if straight is False or count_doubles != 2:
         return False
     else:
         return True
@@ -70,8 +57,14 @@ def increase(password):
         pw[-1] = alphabet[alphabet.index(pw[-1])+1]
     return ''.join(pw)
 
-while input != 'max':
-    input = increase(input)
-    if (checkrules(input)):
-        print(input, 'correct')
-        break
+
+def findnext(password):
+    while password != 'max':
+        password = increase(password)
+        if checkrules(password):
+            return password
+
+
+input = 'cqjxjnds'
+print('solution to part 1:', findnext(input))
+print('solution to part 2:', findnext(findnext(input)))
