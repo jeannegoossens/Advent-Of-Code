a6fb184a06468b977c2455e609d0dec3da1256b7
JMAGoossens <goossens.jma@gmail.com>
2020-12-18 19:58:39 +0100

    solved day 18 part 1

--- 2020/Day18/script.py
+++ 2020/Day18/script.py
@@ -24,9 +24,10 @@ def solution(equation):
 def solve(equation):
     inner = re.findall(r'\([^\)\(]*\)', equation)
     if len(inner) == 1:
-        inner[0] = inner[0][1:-1]
-        # inner = re.findall(r'\([^\)\(]*\)', inner[0])
-    if len(inner) > 0:
+        inner[0] = inner[0].replace('(', '').replace(')', '')
+        equation = equation.replace('(' + inner[0] + ')', str(solution(inner[0])))
+        return solve(equation)
+    elif len(inner) > 1:
         for x in range(len(inner)):
             equation = equation.replace(inner[x], str(solve(inner[x])))
         return solve(equation)
@@ -34,20 +35,8 @@ def solve(equation):
         return solution(equation)
 
 
-# ind = 0
-# total = 0
-# for line in inp:
-#     sol = solve(line)
-#     print(ind, 'equation', line, '=', sol)
-#     total += sol
-#     ind += 1
-# print(total)
-
-# 570016041025 too low
-# 572361518943 too low
-
-inp = "4 + 6 + (3 + 8 * 2 * 6 * 7 + 3)"
-print(solve(inp)) # 937  (1179)
-
-inp = "(3 * 7) + 2 * 3 + 4 + (5 * 8 + 7 + 9 * 7 * 7)"
-print(solve(inp)) # 2817
+total = 0
+for line in inp:
+    sol = solve(line)
+    total += sol
+print('solution to part 1', total)
