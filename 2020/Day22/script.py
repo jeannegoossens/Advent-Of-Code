08acea0ac0685be0db656030ea0b14e6f374a1bb
jeannegoossens <45682783+jeannegoossens@users.noreply.github.com>
2020-12-22 10:41:36 +0100

    day 22 part 2

--- 2020/Day22/script.py
+++ 2020/Day22/script.py
@@ -1,10 +1,7 @@
-input = open('input.txt').read().split('\n\n')
+input = open('inp.txt').read().split('\n\n')
 
-p1 = list(map(int, input[0].split('\n')[1:]))
-p2 = list(map(int, input[1].split('\n')[1:]))
-
-print(p1)
-print(p2)
+deck1 = list(map(int, input[0].split('\n')[1:]))
+deck2 = list(map(int, input[1].split('\n')[1:]))
 
 
 def score(cards):
@@ -16,19 +13,30 @@ def score(cards):
 
 
 def playGame(p1, p2):
-    round = 1
+    past_decks = [{'p1': p1.copy(), 'p2': p2.copy()}]
     while len(p1) != 0 and len(p2) != 0:
         p1_card = p1.pop(0)
         p2_card = p2.pop(0)
 
-        if p1_card > p2_card:
+        if len(p1) >= p1_card and len(p2) >= p2_card:
+            winner, points = playGame(p1[0:p1_card].copy(), p2[0:p2_card].copy())
+        else:
+            if p1_card > p2_card:
+                winner = '1'
+            else:
+                winner = '2'
+
+        if winner == '1':
             p1.append(p1_card)
             p1.append(p2_card)
         else:
             p2.append(p2_card)
             p2.append(p1_card)
 
-        round += 1
+        deck = {'p1': p1.copy(), 'p2': p2.copy()}
+        if deck in past_decks:
+            return '1', score(p1)
+        past_decks.append(deck)
 
     if len(p1) > len(p2):
         return '1', score(p1)
@@ -36,6 +44,6 @@ def playGame(p1, p2):
         return '2', score(p2)
 
 
-winner, points = playGame(p1, p2)
+winner, points = playGame(deck1, deck2)
 
-print('\nSolution to part 1:', points, '(player', winner, 'won)')
+print('Solution to part 2:', points, '(player', winner, 'won)')
