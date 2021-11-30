4b7f46609c8b3906fe7f13448416895be247d1f7
jeannegoossens <45682783+jeannegoossens@users.noreply.github.com>
2021-11-30 18:13:28 +0100

    solved day 17 part 1

--- 2020/Day17/script.py
+++ 2020/Day17/script.py
@@ -6,13 +6,6 @@ grid = open('input.txt').read().split('\n')
 # d . e         l m n           u v w
 # f g h         o p q           x y z
 
-active = []
-for line in range(len(grid)):
-    for c in range(len(grid[0])):
-        if grid[line][c] == '#':
-            active.append((line, c, 0))
-print(active)
-
 
 neighbours = {'a': (-1, -1, 0),  'b': (-1, 0, 0),  'c': (-1, 1, 0),
               'd': (0, -1, 0),                     'e': (0, 1, 0),
@@ -25,6 +18,15 @@ neighbours = {'a': (-1, -1, 0),  'b': (-1, 0, 0),  'c': (-1, 1, 0),
               'r': (-1, -1, 1),  's': (-1, 0, 1),  't': (-1, 1, 1),
               'u': (0, -1, 1),   'v': (0, 0, 1),   'w': (0, 1, 1),
               'x': (1, -1, 1),   'y': (1, 0, 1),   'z': (1, 1, 1)}
+
+
+def getInitialState(grid):
+    active = []
+    for line in range(len(grid)):
+        for c in range(len(grid[0])):
+            if grid[line][c] == '#':
+                active.append((line, c, 0))
+    return active
 
 
 def check_neighbours(active, coordinate, neighbours):
@@ -42,7 +44,6 @@ def generation(active, neighbours):
         n = check_neighbours(old, a, neighbours)
         # first check for all existing actives
         if n == 2 or n == 3:
-            print('a', a)
             new.append(a)
         # then check for all surrounding neighbours of that one
         for nbc in neighbours.values():
@@ -50,28 +51,39 @@ def generation(active, neighbours):
             if neighbour not in old:
                 nbn = check_neighbours(old, neighbour, neighbours)
                 if nbn == 3:
-                    print('n', neighbour)
                     new.append(neighbour)
     return new
 
 
-old = copy.deepcopy(active)
-new = generation(old, neighbours)
-print(len(set(new)), sorted(new))
-print(len(set(new)), sorted(set(new)))
+def printgrid(grid, gen):
+    minx = min([item[0] for item in grid])
+    maxx = max([item[0] for item in grid])
+    miny = min([item[1] for item in grid])
+    maxy = max([item[1] for item in grid])
+    minz = min([item[2] for item in grid])
+    maxz = max([item[2] for item in grid])
+    print('\ngeneration', gen, '(', countActive(grid), ')')
+    for i in range(minz,maxz+1):
+        print('\nz=', i)
+        for j in range(minx, maxx+1):
+            row = ''
+            for k in range(miny, maxy+1):
+                if ((j,k,i) in grid):
+                    row += '#'
+                else:
+                    row += '.'
+            print(row)
 
-# old = copy.deepcopy(grid)
-# # printgrid(old)
-# new = generation(old)
-#
-# i = 0
-# while i < 99:
-#     old = copy.deepcopy(new)
-#     new = generation(old)
-#     i += 1
-#
-# on = 0
-# for x in new:
-#     on += x.count('#')
-#
-# print('solution to part 1:', on)
+
+def countActive(grid):
+    return len(set(grid))
+
+
+active = getInitialState(grid)
+i = 0
+while i < 6:
+    active = generation(active, neighbours)
+    i += 1
+
+print('solution to part 1:', countActive(active))
+# 252 part 1
