77168a7c65a6dfc8e53e174d2879bde0ddcd4bce
jeannegoossens <45682783+jeannegoossens@users.noreply.github.com>
2021-12-01 09:08:56 +0100

    Day 1 pt 2

--- 2021/Day1.py
+++ 2021/Day1.py
@@ -3,9 +3,11 @@ inp = [int(i) for i in open("input.txt").read().split('\n')]
 prev = sum(inp[0:3])
 count = 0
 
-for i in inp[1:]:
-  if i > prev:
-    count += 1
-  prev = i
+for i in range(len(inp[1:])):
+  if i <= len(inp)-2:
+    j = sum(inp[i:i+3])
+    if j > prev:
+      count += 1
+    prev = j
   
 print(count)
