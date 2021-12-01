3b223fbb4799ace7014fb488ad982ea55b049e20
jeannegoossens <45682783+jeannegoossens@users.noreply.github.com>
2021-12-01 12:25:45 +0100

    Day 19 pt 1

--- 2020/Day19/script.py
+++ 2020/Day19/script.py
@@ -1,55 +1,29 @@
-input = open('inp.txt').read().split('\n\n')
+import re
 
-testinput = """0: 4 1 5
-1: 2 3 | 3 2
-2: 4 4 | 5 5
-3: 4 5 | 5 4
-4: "a"
-5: "b"
+inp = open('input.txt').read().split('\n\n')
 
-ababbb
-bababa
-abbbab
-aaabbb
-aaaabbb""".split('\n\n')
+rulebook = {r.split(': ')[0]: r.split(': ')[1] for r in inp[0].split('\n')}
+messages = inp[1].split('\n')
 
-rules = {r.split(': ')[0]: r.split(': ')[1] for r in testinput[0].split('\n')}
-messages = testinput[1].split('\n')
+def has_numbers(inputString):
+    return any(char.isdigit() for char in inputString)
 
-print(rules)
-print(messages)
-
-
-def read_rule(rule_id, rulebook):
-    rule = rulebook[rule_id]
-    rule = rule.split(' | ')
-    rule = [x.split(' ') for x in rule]
-    print(rule_id, rule)
+def decodeRule(rulebook, rule):
+    while (has_numbers(rule)):
+        for r in rule.split(' '):
+            if (r.isnumeric()):
+                pattern = r'\D'+ r + r'\D'
+                rule = re.sub(pattern, ' ( ' + rulebook[r] + ' ) ', ' '+rule+' ').strip()
     return rule
 
+rule = decodeRule(rulebook, rulebook['0'])
+rule = rule.replace(' ', '').replace('")', '').replace('("', '')
 
-def apply_rule(rule_id, rulebook, message, index):
-    rule = read_rule(rule_id, rulebook)
-    for option in rule:
-        for rule_id in option:
-            print(type(rule_id))
-            if rule_id in ["a", "b"]:
-                print('rule', rule_id, 'is character')
-                if message[index] == rule_id:
-                    return 1
-                else:
-                    return 0
-            else:
-                print('applying rule', rule_id, 'to message', message, 'at index', index, '(', message[index:], ')')
-                index += apply_rule(rule_id, rulebook, message, index)
+count = 0
+pattern = rule + '$'
+for m in messages:
+    if re.match(pattern, m):
+        count += 1
+print(count)
 
-
-def checkMessage(message, rulebook):
-    index = 0
-    apply_rule('0', rulebook, message, index)
-
-
-for message in messages:
-    print(message)
-    checkMessage(message, rules)
-    break
+# 113 part 1
