03e28b72c14bb6a62873380003fae20707c4f7c5
jeannegoossens <goossens.jma@gmail.com>
2021-12-06 08:14:57 +0100

    Day 6

--- 2021/Day6.py
+++ 2021/Day6.py
@@ -1,17 +1,21 @@
 fish = [int(i) for i in open("inputs/day6.txt").read().split(',')]
+fish = {i: fish.count(i) for i in range(9)}
 
 
 def runday(fish):
-    newday = []
-    for f in fish:
-        if f > 0:
-            newday.append(f-1)
-        elif f == 0:
-            newday.append(6)
-            newday.append(8)
-    return newday
+    fish6 = fish[6]
+    fish[6] = fish[7] + fish[0]
+    fish[7] = fish[8]
+    fish[8] = fish[0]
+    fish[0] = fish[1]
+    fish[1] = fish[2]
+    fish[2] = fish[3]
+    fish[3] = fish[4]
+    fish[4] = fish[5]
+    fish[5] = fish6
+    return fish
 
 
-for i in range(80):
+for i in range(256):
     fish = runday(fish)
-print('part 1', len(fish))
+print(sum(fish.values()))
