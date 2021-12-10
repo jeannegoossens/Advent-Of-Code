9eb1c71e5e43d5bf46a01c6004f06f55fdc9c411
jeannegoossens <goossens.jma@gmail.com>
2021-12-10 15:51:50 +0100

    restructure directory

--- 2019/IntCode/Day05.py
+++ 2019/IntCode/Day05.py
@@ -3,57 +3,52 @@
 # Part 1: 2019 day 02
 
 
-def addition(input, first, second):
-    return input[first] + input[second]
-
-
-def multiplication(input, first, second):
-    return input[first] * input[second]
-
-
-def takeInput(intcode, first):
-    return first
-
-
-def giveOutput(intcode, first):
-    return intcode[first]
-
-
-def run(intcode, noun, verb):
-    intcode[1] = noun
-    intcode[2] = verb
-    i = 0
-    while intcode[i] != 99:
-        opcode = intcode[i]
-        if opcode == 1:
-            intcode[intcode[i+3]] = addition(intcode, intcode[i+1], intcode[i+2])
-            i += 4
-        elif opcode == 2:
-            intcode[intcode[i+3]] = multiplication(intcode, intcode[i+1], intcode[i+2])
-            i += 4
-        elif opcode == 3:
-            intcode[intcode[i+1]] = 000
-            i += 2
-        elif opcode == 4:
-            giveOutput(intcode, intcode[i+1])
-            i += 2
-        else:
-            print(intcode[i:i+4])
-            i += 4
+def run(intcode, inputvalue = 5):
+    opcode = 0
+    while intcode[opcode] != 99:
+        instruction = str(intcode[opcode]).zfill(5)
+        op = int(instruction[-2:])
+        p1mode = int(instruction[-3])
+        p2mode = int(instruction[-4])
+        p3mode = int(instruction[-5])
+        if intcode[opcode] == 1:
+            p1 = intcode[intcode[opcode+1]] if p1mode == 0 else intcode[opcode+1]
+            p2 = intcode[intcode[opcode+2]] if p2mode == 0 else intcode[opcode+2]
+            if p3mode == 0:
+                intcode[intcode[opcode+3]] = p1 + p2
+            else:
+                intcode[opcode+3] = p1 + p2
+            opcode += 4
+        elif intcode[opcode] == 2:
+            p1 = intcode[intcode[opcode+1]] if p1mode == 0 else intcode[opcode+1]
+            p2 = intcode[intcode[opcode+2]] if p2mode == 0 else intcode[opcode+2]
+            if p3mode == 0:
+                intcode[intcode[opcode+3]] = p1 * p2
+            else:
+                intcode[opcode+3] = p1 * p2
+            opcode += 4
+        elif intcode[opcode] == 3:
+            if p1mode == 0:
+                intcode[intcode[opcode+1]] = inputvalue
+            else:
+                intcode[opcode+1] = inputvalue
+            opcode += 2
+        elif intcode[opcode] == 4:
+            if p1mode == 0:
+                print(intcode[intcode[opcode+1]])
+            else:
+                print(intcode[opcode+1])
+            opcode += 2
     return intcode[0]
 
 
-input = list(map(int, open('inputs/02.txt').read().split(',')))
+inp = [int(i) for i in open('inputs/05.txt').read().split(',')]
+print(inp)
 
-# part 1
-noun = 12
-verb = 2
-print(run(input.copy(), noun, verb))
 
-# part 2
-for noun in range(0, 99):
-    for verb in range(0, 99):
-        intcode = input.copy()
-        result = run(intcode, noun, verb)
-        if result == 19690720:
-            print(100*noun + verb)
+def findanswer():
+    intcode = [int(i) for i in open('inputs/05.txt').read().split(',')]
+    #intcode = [1002,4,3,4,33]
+    inputnumber = int(input('What id? '))
+    answer = run(intcode, inputnumber)
+print(findanswer())
