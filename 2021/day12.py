724e8100955a580d6df5b4e7b355de40c38ca250
jeannegoossens <goossens.jma@gmail.com>
2021-12-12 12:11:03 +0100

    Day 12

--- 2021/day12.py
+++ 2021/day12.py
@@ -1,5 +1,5 @@
 import networkx as nx
-import matplotlib.pyplot as plt  # to draw the graph
+import matplotlib.pyplot as plt
 
 inp = [sorted(i.split('-')) for i in open('inputs/day12.txt').read().split('\n')]
 
@@ -7,7 +7,7 @@ G = nx.Graph()
 for i in inp:
     G.add_edge(i[0], i[1])
 
-paths = [['start']]
+smallcaves = [cave for cave in G.nodes() if cave.islower()]
 
 
 def check_for_unfinished_paths(paths):
@@ -17,19 +17,39 @@ def check_for_unfinished_paths(paths):
     return False
 
 
+def check_node_allowed(path, node):
+    if not node.islower():
+        return True
+    else:
+        if node == 'start':
+            return False
+
+        # count how many times this new node already appears
+        if path.count(node) >= 2:
+            return False
+
+        if len([cave for cave in smallcaves if path.count(cave) == 2]) > 0:
+            # there already is a small cave that was visited twice
+            return path.count(node) < 1
+        else:
+            return True
+
+
+paths = [['start']]
+
 while check_for_unfinished_paths(paths):
     for path_idx in range(len(paths)):
         if path_idx < len(paths):
             path = paths[path_idx]
             if path[-1] != 'end':
                 for next_node in G[path[-1]]:
-                    if not next_node.islower() or (next_node.islower() and next_node not in path):
+                    if check_node_allowed(path, next_node):
                         newpath = path.copy()
                         newpath.append(next_node)
                         paths.append(newpath)
                 paths.remove(path)
 
-print('part 1:', len(paths))
+print('part 2:', len(paths))
 
 
 # draw the graph visually
