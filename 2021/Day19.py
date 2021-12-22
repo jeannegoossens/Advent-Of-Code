4df940b5882e7a5142bfc965004e0317d32dfdd8
jeannegoossens <goossens.jma@gmail.com>
2021-12-22 13:34:57 +0100

    Day 19

--- 2021/Day19.py
+++ 2021/Day19.py
@@ -1,28 +1,49 @@
-# scanners = open('inputs/day19.txt').read().split('\n\n')
-scanners = open('testinput.txt').read().split('\n\n')
+scanners = open('inputs/day19.txt').read().split('\n\n')
+# scanners = open('testinput.txt').read().split('\n\n')
 
-inputscanners = [[(int(beacon.split(',')[0]), int(beacon.split(',')[1]), int(beacon.split(',')[2])) for beacon in s.split('\n')[1:]] for s in scanners]
+inputscanners = [[(int(beacon.split(',')[0]), int(beacon.split(',')[1]), int(beacon.split(',')[2]))
+                  for beacon in s.split('\n')[1:]] for s in scanners]
 
 
 class Scanner:
     def __init__(self, id, beacons):
         self.id = id
+        self.relative_beacons = [Beacon(beacon) for beacon in beacons]
+        self.absolute_beacons = None
         self.location = None
-        self.beacons = {beacon: Beacon(beacon) for beacon in beacons}
-        for beacon in self.beacons.values():
-            self.calc_distances(beacon)
-        self.translation = None
-        self.orientation = None
+        self.calc_manhattan_distances()
 
     def __repr__(self):
-        return f"Scanner {self.id} at {self.location} with {len(self.beacons)} beacons, oriented {self.orientation} at translation {self.translation}"
+        return f"Scanner {self.id} {self.location} with {len(self.relative_beacons)} beacons."
 
-    def calc_distances(self, beacon):
-        beacon.distances = [beacon.distance_to(other) for other in self.beacons.values()]
+    def set_absolute_beacons(self, orientation, displacement):
+        self.absolute_beacons = []
+        for rel_beacon in self.relative_beacons:
+            abs_beacon = Beacon(rel_beacon.coordinates())
 
-    def reorient_beacons(self, orientation_idx):
-        for beacon in self.beacons.values():
-            beacon.reorient(orientation_idx)
+            # apply the reorientation
+            reoriented = abs_beacon.orientations()[orientation]
+            abs_beacon.x = reoriented[0]
+            abs_beacon.y = reoriented[1]
+            abs_beacon.z = reoriented[2]
+
+            # now apply the translation
+            abs_beacon.x += displacement.coordinates()[0]
+            abs_beacon.y += displacement.coordinates()[1]
+            abs_beacon.z += displacement.coordinates()[2]
+
+            self.absolute_beacons.append(abs_beacon)
+
+            # set scanner location
+            if self.location is None:
+                self.location = (abs_beacon - Beacon(rel_beacon.orientations()[orientation])).coordinates()
+
+        for beacon in self.absolute_beacons:
+            beacon.distances = [beacon.distance_to(other) for other in self.absolute_beacons]
+
+    def calc_manhattan_distances(self):
+        for beacon in self.relative_beacons:
+            beacon.distances = [beacon.distance_to(other) for other in self.relative_beacons]
 
 
 class Beacon:
@@ -30,52 +51,10 @@ class Beacon:
         self.x = coordinates[0]
         self.y = coordinates[1]
         self.z = coordinates[2]
-        self.ox = coordinates[0]
-        self.oy = coordinates[1]
-        self.oz = coordinates[2]
         self.distances = None
 
     def __repr__(self):
         return f"Beacon ({self.x}, {self.y}, {self.z})"
-
-    def orientations(self):
-        return [
-            (self.ox, self.oy, self.oz),
-            (self.ox, -self.oy, -self.oz),  # upside down (forwards 2x)
-            (-self.ox, self.oy, -self.oz),  # turned clockwise 2x
-            (-self.ox, -self.oy, self.oz),  # dropped right 2x
-
-            (self.ox, self.oz, -self.oy),  # dropped backwards
-            (self.ox, -self.oz, self.oy),  # dropped forwards
-            (-self.ox, self.oz, self.oy),  # flipped, dropped forwards
-            (-self.ox, -self.oz, -self.oy),  # flipped, dropped backwards
-
-            (self.oy, self.ox, -self.oz),  # flipped, dropped right
-            (self.oy, -self.ox, self.oz),  # dropped right
-            (-self.oy, self.ox, self.oz),  # dropped left
-            (-self.oy, -self.ox, -self.oz),  # flipped, dropped left
-
-            (self.oy, self.oz, self.ox),  # dropped right, dropped backwards
-            (self.oy, -self.oz, -self.ox),  # dropped right, dropped forwards
-            (-self.oy, self.oz, -self.ox),  # dropped left, dropped backwards
-            (-self.oy, -self.oz, self.ox),  # dropped left, dropped forwards
-
-            (self.oz, self.ox, self.oy),  # turned counterclockwise, dropped forwards
-            (self.oz, -self.ox, -self.oy),  # turned counterclockwise, dropped backwards
-            (-self.oz, self.ox, -self.oy),  # turned clockwise, dropped backwards
-            (-self.oz, -self.ox, self.oy),  # turned clockwise, dropped forwards
-
-            (self.oz, self.oy, -self.ox),  # turned counterclockwise
-            (self.oz, -self.oy, self.ox),  # turned counterclockwise, upside down (dropped backwards 2x)
-            (-self.oz, self.oy, self.ox),  # turned clockwise
-            (-self.oz, -self.oy, -self.ox),  # turned clockwise, upside down (dropped forwards 2x)
-        ]
-
-    def reorient(self, orientation_idx):
-        reorientation = self.orientations()[orientation_idx]
-        self.x = reorientation[0]
-        self.y = reorientation[1]
-        self.z = reorientation[2]
 
     def __add__(self, other):
         return Beacon((self.x + other.x, self.y + other.y, self.z + other.z))
@@ -89,85 +68,120 @@ class Beacon:
     def distance_to(self, other):
         return abs(self.x - other.x) + abs(self.y - other.y) + abs(self.z - other.z)
 
+    def orientations(self):
+        return [
+            (self.x, self.y, self.z),
+            (self.x, -self.y, -self.z),   # upside down (forwards 2x)
+            (-self.x, self.y, -self.z),   # turned clockwise 2x
+            (-self.x, -self.y, self.z),   # dropped right 2x
+
+            (self.x, self.z, -self.y),    # dropped backwards
+            (self.x, -self.z, self.y),    # dropped forwards
+            (-self.x, self.z, self.y),    # flipped, dropped forwards
+            (-self.x, -self.z, -self.y),  # flipped, dropped backwards
+
+            (self.y, self.x, -self.z),    # flipped, dropped right
+            (self.y, -self.x, self.z),    # dropped right
+            (-self.y, self.x, self.z),    # dropped left
+            (-self.y, -self.x, -self.z),  # flipped, dropped left
+
+            (self.y, self.z, self.x),     # dropped right, dropped backwards
+            (self.y, -self.z, -self.x),   # dropped right, dropped forwards
+            (-self.y, self.z, -self.x),   # dropped left, dropped backwards
+            (-self.y, -self.z, self.x),   # dropped left, dropped forwards
+
+            (self.z, self.x, self.y),     # turned counterclockwise, dropped forwards
+            (self.z, -self.x, -self.y),   # turned counterclockwise, dropped backwards
+            (-self.z, self.x, -self.y),   # turned clockwise, dropped backwards
+            (-self.z, -self.x, self.y),   # turned clockwise, dropped forwards
+
+            (self.z, self.y, -self.x),    # turned counterclockwise
+            (self.z, -self.y, self.x),    # turned counterclockwise, upside down (dropped backwards 2x)
+            (-self.z, self.y, self.x),    # turned clockwise
+            (-self.z, -self.y, -self.x),  # turned clockwise, upside down (dropped forwards 2x)
+        ]
+
     def coordinates(self):
         return (self.x, self.y, self.z)
 
 
-# Loading all scanners into a dictionary based on scanner id
+# make Scanner objects from the input
 scanners = {}
-for i in range(len(inputscanners)):
-    s = Scanner(i, inputscanners[i])
-    scanners[i] = s
-scanners[0].location = (0,0,0)
-
-# Track for which scanners we already know the absolute positions
-known_scanners = [0]
+for line in range(len(inputscanners)):
+    scanners[line] = Scanner(line, inputscanners[line])
+scanners[0].location = (0, 0, 0)
+scanners[0].set_absolute_beacons(0, Beacon((0, 0, 0)))
 
 
-# Find matching beacons between scanners based on their absolute distances to other beacons
-def find_pairs():
-    # print("Find new pairs. We already know scanners:", known_scanners)
-    for known_scanner in known_scanners:
-        known_scanner = scanners[known_scanner]
-        for scanner in scanners.values():
-            if scanner.id not in known_scanners:
-                # print("\tSearching for scanner", scanner.id, "on known scanner", known_scanner.id)
-                pairs = []
-                for beacon in known_scanner.beacons.values():
-                    for other_beacon in scanner.beacons.values():
-                        if len(list(set(beacon.distances) & set(other_beacon.distances))) >= 11:
-                            pairs.append((beacon, other_beacon))
-                if len(pairs) >= 11:
-                    print(f"\nFound {len(pairs)} pairs between known scanner {known_scanner.id} and scanner {scanner.id}")
-                    known_scanners.append(scanner.id)
-                    return pairs, known_scanner.id, scanner.id
+# track for which scanners we have identified the absolut positions
+absolute_scanners = [0]
 
 
-# Now for each orientation of the pairs from the scanner we have matched, we need to check whether the translation between all pairs is equal to find the correct orientation of the scanner
-# for any pair (we take the first one for simplicity)
-#     for each orientation of the second beacon of that pair
-#         calculate the displacement of the second beacon
-#         for every beacon in the scanner we are matching
-#             calculate what absolute position it woud have if in the same orientation and with the same displacement as the second beacon in the pair
-#             if that position occurs in the known grid, we have found a matching beacon
-#         if we have found enough matches, we know the right orientation for this scanner
-def matchOrientation(pairs, known_scanner, scanner_id):
-    # print("Find matching orientation of the newest scanner on existing scanner:", known_scanner, scanner_id)
-    pair = pairs[0]
-    for idx, orientation in enumerate(pair[1].orientations()):
+def find_pairs(first: Scanner, second: Scanner):
+    pairs = []
+    for first_beacon in first.absolute_beacons:
+        for second_beacon in second.relative_beacons:
+            if len(list(set(second_beacon.distances) & set(first_beacon.distances))) >= 11:
+                pairs.append((first_beacon, second_beacon))
+    if len(pairs) >= 11:
+        return pairs
+    return None
+
+
+def locate_scanner():
+    # for any unidentified scanner
+    for search_scanner in scanners.values():
+        if search_scanner.id not in absolute_scanners:
+            # for all identified scanners
+            for s_id in absolute_scanners:
+                known_scanner = scanners[s_id]
+                # see if there are pairs to be found between these
+                pairs = find_pairs(known_scanner, search_scanner)
+                if pairs:
+                    return search_scanner.id, known_scanner.id, pairs
+
+
+def match_orientation(pairs, known_scanner_id, new_scanner_id):
+    # we can do this with any pair we found so we will take the first one
+    matchpair = pairs[0]
+
+    # loop through all orientations a beacon can have
+    for index, orientation in enumerate(matchpair[1].orientations()):
         matches = 0
-        displacement = pair[0] - Beacon(orientation)
-        for beacon in scanners[scanner_id].beacons.values():
-            displaced = Beacon(beacon.orientations()[idx]) + displacement
-            if displaced in scanners[known_scanner].beacons.values():
+        # calculate how far the beacon needs to be moved in this orientation
+        # to match the other beacons absolute position
+        displacement = matchpair[0] - Beacon(orientation)
+        for relative_beacon in scanners[new_scanner_id].relative_beacons:
+            displaced_beacon = Beacon(relative_beacon.orientations()[index]) + displacement
+            if displaced_beacon in scanners[known_scanner_id].absolute_beacons:
                 matches += 1
         if matches >= 11:
-            # print("\tMatch found at orientation", idx)
-            return idx
-    # print("No matching orientation found?")
+            return index, displacement
 
 
-# Now we know the right orientation of this new scanner, we can turn all beacons into that orientation, then calculate their absolute positions, and add those to the known beacon locations
-# We can calculate their absolute positions by taking their relative position and adding the distance between the first pairs both beacons
-def locate_scanner():
-    # print("Locate the next scanner")
-    pairs, known_scanner, scanner_id = find_pairs()
-    correct_orientation = matchOrientation(pairs, known_scanner, scanner_id)
-    scanners[scanner_id].reorient_beacons(correct_orientation)
-    scanners[scanner_id].orientation = correct_orientation
-    displacement = pairs[0][1] - pairs[0][0]
-    scanners[scanner_id].translation = displacement
-    for beacon in scanners[scanner_id].beacons.values():
-        beacon.x += displacement.x
-        beacon.y += displacement.y
-        beacon.z += displacement.z
+def match_scanners():
+    while len(absolute_scanners) < len(scanners.keys()):
+        new_scanner_id, known_scanner_id, pairs = locate_scanner()
+        orientation, displacement = match_orientation(pairs, known_scanner_id, new_scanner_id)
+        absolute_scanners.append(new_scanner_id)
+        scanners[new_scanner_id].set_absolute_beacons(orientation, displacement)
     return
 
 
-# And then we need to do ALL of that until we found each scanner
-while len(known_scanners) < len(scanners.keys()):
-    locate_scanner()
+match_scanners()
 
-print()
+unique_beacons = set()
 for scanner in scanners.values():
-    print(scanner)
+    for absolute_beacon in scanner.absolute_beacons:
+        unique_beacons.add(absolute_beacon.coordinates())
+
+print("Part 1:", len(unique_beacons))
+# part 1: 512
+
+max_manhatten = 0
+for s1 in scanners.values():
+    for s2 in scanners.values():
+        manhattan = abs(s1.location[0] - s2.location[0]) + abs(s1.location[1] - s2.location[1]) + abs(s1.location[2] - s2.location[2])
+        max_manhatten = max(max_manhatten, manhattan)
+print("Part 2:", max_manhatten)
+# part 2: 16802
