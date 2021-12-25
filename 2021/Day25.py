95588b0026b83ba9b778887f06b8751a20caddca
jeannegoossens <45682783+jeannegoossens@users.noreply.github.com>
2021-12-25 16:23:21 +0100

    Day 25

--- 2021/Day25.py
+++ 2021/Day25.py
@@ -1,5 +1,5 @@
 # inp = open('inputs/day25.txt').read().split('\n')
-inp = open('testinput.txt').read().split('\n')
+inp = open('input.txt').read().split('\n')
 
 east_cucumbers = set()
 south_cucumbers = set()
@@ -8,25 +8,29 @@ boardwidth = len(inp[0])
 boardheight = len(inp)
 
 for y in range(len(inp)):
-    for x in range(len(inp)):
+    for x in range(len(inp[0])):
         if inp[y][x] == '>':
             east_cucumbers.add((y,x))
         elif inp[y][x] == 'v':
             south_cucumbers.add((y,x))
 
+print(sorted(east_cucumbers))
+print(sorted(south_cucumbers))
 
 def generation(east, south):
+    # move east facing sea cucumbers
     east_movers = set()
     for cucumber in east:
         if (cucumber[0], (cucumber[1]+1)%boardwidth) not in east and (cucumber[0], (cucumber[1]+1)%boardwidth) not in south:
             east_movers.add(cucumber)
-    for cucumber in east:
-        if cucumber in east_movers:
-            east.remove(cucumber)
-            east.add((cucumber[0], (cucumber[1]+1)%boardwidth))
+    for cucumber in east_movers:
+        east.remove(cucumber)
+        east.add((cucumber[0], (cucumber[1]+1)%boardwidth))
+    
+    # move south facing sea cucumbers
     south_movers = set()
     for cucumber in south:
-        if (cucumber[0], (cucumber[1]+1)%boardwidth) not in east and (cucumber[0], (cucumber[1]+1)%boardwidth) not in south:
+        if ((cucumber[0]+1)%boardheight, cucumber[1]) not in east and ((cucumber[0]+1)%boardheight, cucumber[1]) not in south:
             south_movers.add(cucumber)
     if len(east_movers) + len(south_movers) == 0:
         return False, False
@@ -36,10 +40,11 @@ def generation(east, south):
     return east, south
 
 
+# print(east_cucumbers, "\n", south_cucumbers)
 step = 0
-while east_cucumbers and south_cucumbers:
+
+while east_cucumbers != False and south_cucumbers != False:
     east_cucumbers, south_cucumbers = generation(east_cucumbers, south_cucumbers)
     step += 1
-    print(step)
 
 print(step)
