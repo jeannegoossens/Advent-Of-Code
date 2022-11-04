a8c6f45932dd26780fc85dc583129c41582cc3d2
jeannegoossens <goossens.jma@gmail.com>
2022-11-04 09:22:08 +0100

    2021 day 24 part 1

--- 2021/Day24.py
+++ 2021/Day24.py
@@ -1,30 +1,30 @@
-# i    0    1    2    3    4    5    6    7    8    9   10   11   12   13
-Z = [  1,   1,   1,   1,   1,  26,   1,  26,  26,   1,  26,  26,  26,  26]  # z div
-X = [ 10,  12,  10,  12,  11, -16,  10, -11, -13,  13,  -8,  -1,  -4, -14]  # x add
-Y = [ 12,   7,   8,   8,  15,  12,   8,  13,   3,  13,   3,   9,   4,  13]  # y add
-# w = [  9,   9,   9,   9,   9,   9,   9,   9,   9,   9,   9,   9,   9,   9]  # w input
+type1 = [0, 1, 2,  3,  4,  6, 9]
+type2 = [5, 7, 8, 10, 11, 12, 13]
+
+params = [[10,12], [12,7], [10,8], [12,8], [11,15], [-16,12], [10,8], [-11,13], [-13,3], [13,13], [-8,3], [-1,9], [-4,4], [-14,13]]
 
 
-def run_program(w):
-    w = [int(i) for i in w]
-    x, y, z = 0, 0, 0
-    for instr in range(14):
-        x = 0 if ((z % 26) + X[instr]) == int(w[instr]) else 1
-        y = (25 if x == 1 else 0) + 1
-        z = int(z / Z[instr]) * y
-        z = z + ((int(w[instr]) + Y[instr]) if x == 1 else 0)
-    return z == 0
+def solve(digit, z):
+    global sol
+    if digit > 13:
+        return z == 0
+    if digit in type2:
+        w = (z % 26) + params[digit][0]
+        thenz = int(z/26)
+        if w < 1 or w > 9:
+            return False
+        else:
+            sol[digit] = w
+            return solve(digit+1, thenz)
+    else:
+        for w in range(9, 0, -1):
+            thenz = (26 * z) + w + params[digit][1]
+            if solve(digit + 1, thenz):
+                sol[digit] = w
+                return True
 
 
-modelnr = 99997258454321
-result = False
-while not result:
-    result = run_program(str(modelnr))
-    if result:
-        print(modelnr, result)
-        break
-    if modelnr % 100000 == 54321:
-        print(modelnr)
-    modelnr -= 1
-    while '0' in str(modelnr):
-        modelnr -= 1
+sol = [0 for _ in range(14)]
+solve(0, 0)
+print(''.join(map(str, sol)))
+
