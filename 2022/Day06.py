ade110d2d9d0839eed15390c162212821e46684e
jeannegoossens <45682783+jeannegoossens@users.noreply.github.com>
2022-12-06 06:23:12 +0100

    2022 Day 06

--- 2022/Day06.py
+++ 2022/Day06.py
@@ -1,13 +1,8 @@
 inp = list(open('inputs/Day06.txt').read())
 
-unique = 14  # or 14 for part 2
-
-last = inp[:unique]
+unique = 4  # or 14 for part 2
 
 for idx in range(len(inp)):
-    if len(set(last)) == unique:
-        print(idx)
+    if len(set(inp[idx:idx+unique])) == unique:
+        print(idx+unique)
         break
-    else:
-        last.pop(0)
-        last.append(inp[idx])
