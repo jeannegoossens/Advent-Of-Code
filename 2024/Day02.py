0ac8faf3a520c78c6d6939e09c84f27ada6f8faf
Jeanne Goossens <goossens.jma@gmail.com>
2024-12-02 22:04:37 +0100

    2024 Day 02

--- 2024/Day02.py
+++ 2024/Day02.py
@@ -1,4 +1,40 @@
-inp = [i for i in open('inputs/Day02.txt').read().split('\n')]
+inp = [[int(x) for x in i.split()] for i in open('inputs/Day02.txt').read().split('\n')]
 
-print("Part 1: ")
-print("Part 2: ")
+
+def check_safe(report):
+    increase = report[0] < report[1]
+    last = report[0]
+    safe = True
+
+    for level in report[1:]:
+        if level == last:
+            safe = False
+        if abs(level - last) > 3 or abs(level - last) < 1:
+            safe = False
+        if (last < level) != increase:
+            safe = False
+        last = level
+
+    return safe
+
+
+def check_safe_dampened(report):
+    allowance = 0
+    for i in range(len(report)):
+        without = report.copy()
+        without.pop(i)
+        allowance += check_safe(without)
+    if allowance == 0:
+        return False
+    return True
+
+
+countSafe = 0
+countSafeDampened = 0
+
+for report in inp:
+    countSafe += check_safe(report)
+    countSafeDampened += check_safe_dampened(report)
+
+print("Part 1:", countSafe)
+print("Part 2:", countSafeDampened)
---
+++ 2024/Day03.py
@@ -0,0 +1,4 @@
+inp = [i for i in open('inputs/Day03.txt').read().split('\n')]
+
+print("Part 1:")
+print("Part 2:")
